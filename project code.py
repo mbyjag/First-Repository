8a7f3df30b1bc04f7c7b84257368d93ed6db73d4
mbyjag <73232720+mbyjag@users.noreply.github.com>
2020-11-12 14:35:32 +0000

    Update project code.py

--- project code.py
+++ project code.py
@@ -1,21 +1,17 @@
-filename = "FAL00432_af307028575a34db3268b4df36412691cb25b558_0.fastq"
-file_data = open(filename, "r")
-file_100 = open("file100.txt", 'w')
-file_200 = open("file200.txt", 'w')
-file_500 = open("file500.txt", 'w')
-counter = 0
-for line in file_data:
-    counter += 1
-    if (counter - 2) % 4 == 0 or (counter % 4 == 0):
-        file_100.write(line[:100]+"\n")
-        file_200.write(line[:200]+"\n")
-        file_500.write(line[:500]+"\n")
-    else:
-        file_100.write(line)
-        file_200.write(line)
-        file_500.write(line)
-
-file_data.close()
-file_100.close()
-file_200.close()
-file_500.close()
+from Bio.SeqIO.QualityIO import FastqGeneralIterator
+count = 0
+with open("file.fastq", "r") as data:
+    with open("file100q.txt", "w") as file100q:
+        with open("file200q.txt", "w") as file200q:
+            with open("file500q.txt", "w") as file500q:
+                with open("file100a.txt", "w") as file100a:
+                    with open("file200a.txt", "w") as file200a:
+                        with open("file500a.txt", "w") as file500a:
+                            for title, seq, qual in FastqGeneralIterator(data):
+                                count += 1
+                                file100q.write(title + "\n" + seq[:100] + "\n" + "+" + "\n" + qual[:100] + "\n")
+                                file200q.write(title + "\n" + seq[:200] + "\n" + "+" + "\n" + qual[:200] + "\n")
+                                file500q.write(title + "\n" + seq[:500] + "\n" + "+" + "\n" + qual[:500] + "\n")
+                                file100a.write(title + "\n" + seq[:100] + "\n")
+                                file200a.write(title + "\n" + seq[:200] + "\n")
+                                file500a.write(title + "\n" + seq[:500] + "\n")
